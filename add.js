0bf2a56cbc87e7c2247e58dc6e91907f8b4567c8
Mucyo Papy Blaise <mucyoblaise86@gmail.com>
2024-05-15 14:13:59 +0200

    Project added

---
+++ add.js
@@ -0,0 +1,18 @@
+const myText = document.getElementById("myText");
+const Reduce = document.getElementById("Reduce");
+const Reset = document.getElementById("Reset");
+const Add = document.getElementById("Add");
+let num = 0;
+
+const reduce=()=>{
+    num--;
+    myText.textContent = num;
+}
+const add=()=>{
+    num++;
+    myText.textContent = num;
+}
+const reset=()=>{
+    num = 0;
+    myText.textContent = num;
+}
